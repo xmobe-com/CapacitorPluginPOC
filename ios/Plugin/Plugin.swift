0f4522d819576dac1eb6cfd02260ab52aaef0db7
Le Ngoc Duy <askbills@gmail.com>
2020-08-06 15:35:47 +0700

    chore(log): add log to help debug

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -9,12 +9,13 @@ import Capacitor
 public class LoginPlugin: CAPPlugin {
     
     @objc func showLogin(_ call: CAPPluginCall) {
-        #if DEBUG
+//        #if DEBUG
         print("Javascript -> invoke Native API")
-        #endif
+//        #endif
         
         let value = call.getString("value") ?? ""
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        print(storyboard)
         DispatchQueue.main.async {
             let controller = storyboard.instantiateViewController(withIdentifier: "CustomViewController") as? CustomViewController
             UIApplication.shared.windows.first!.rootViewController = UINavigationController(rootViewController: controller!)
